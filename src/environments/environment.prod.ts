988062b274714e28cce0aa3d21eacec3a417cf0d
PeteHass94 <phassard@hotmail.co.uk>
2021-04-26 02:50:53 +0100

    back to localhost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-   //apiURL: "http://localhost:3000/api"
-  apiURL: "https://fyp-football-cms1.herokuapp.com/api"
+  apiURL: "http://localhost:3000/api"
+  //apiURL: "https://fyp-football-cms1.herokuapp.com/api"
 };
